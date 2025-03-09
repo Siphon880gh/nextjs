5c17758e7ae3f3e306839635124228dba5ad83fa
Siphon880gh <weng.f.fung@gmail.com>
2025-03-09 00:26:57 -0800

    generateStaticParams

--- app/help/issue/[issue]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page({ params }: { params: { issue: string } }) {
-    return (
-        <div>
-            Your help ticket <b>#{params.issue}</b>.
-            ...
-        </div>
-    )
-}
